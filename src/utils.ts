b77fbcef75eb0be070c52b4d2ed5afea7dfa39d9
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-14 18:33:17 +0530

    Create utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+// export const getActiveTodosFromLocalStorage = () => {
+//   let todosList = localStorage.getItem("activeTodos");
+//   if (todosList) {
+//     return todosList;
+//   } else {
+//     return [];
+//   }
+// };
+
+// export const getCompletedTodosFromLocalStorage = () => {
+//   let todosList = localStorage.getItem("completedTodos");
+//   if (todosList) {
+//     return todosList;
+//   } else {
+//     return [];
+//   }
+// };
