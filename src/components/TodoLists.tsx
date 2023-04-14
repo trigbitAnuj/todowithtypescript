a9282feb8abb81a153abea0811f7f55726a3fc0e
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-14 18:33:08 +0530

    Create TodoLists.tsx

---
+++ src/components/TodoLists.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+import { Draggable, Droppable } from "react-beautiful-dnd";
+import { Todo } from "../pages/TodoApp";
+import { AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+
+
+type Props={
+  todos: Todo[],
+  handleChangeTodo:(id:number)=>void;
+  handleDeleteTodo:(id:number)=>void;
+  handleCompleteChangeTodo:(id:number)=>void 
+  handleCompleteDeleteTodo:(id:number)=>void
+  completedTodos:Todo[]
+  
+}
+
+type SingleTodoProps ={
+  key:number
+  index:number
+  todo:Todo
+  handleChangeTodo:(id:number)=>void
+  
+  handleDeleteTodo:(id:number)=>void
+
+  
+}
+
+
+
+
+const TodoLists = ({todos,handleChangeTodo,handleDeleteTodo,completedTodos,handleCompleteChangeTodo,handleCompleteDeleteTodo}:Props)=> {
+
+  
+  return (
+    <section className=" flex  w-full mt-4 justify-between gap-2">
+
+      <Droppable droppableId="todos-list">
+        {(provided) => (
+          <section
+            className="flex flex-col  bg-teal-300 p-[10px] w-[50%]  rounded-lg gap-2 "
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <h1 className="text-3xl text-white font-serif text-center">
+              Active Todos
+            </h1>
+
+            {todos.map((todo, index) => (
+              // <li>{todo.text}</li>
+              <SingleTodo
+                key={todo.id}
+                index={index}
+                todo={todo}
+                handleChangeTodo={handleChangeTodo}
+                handleDeleteTodo={handleDeleteTodo}
+              />
+            ))}
+
+            {provided.placeholder}
+          </section>
+        )}
+      </Droppable>
+
+       <Droppable droppableId="Todos-remove">
+        {(provided) => (
+          <section
+            className="flex flex-col bg-red-400 w-[50%]    rounded-lg p-[10px]  gap-2"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <h1 className="text-3xl font-serif text-white text-center">
+              Completed Todos
+            </h1>
+
+            {completedTodos.map((todo, index) => (
+              <SingleTodo
+                key={todo.id}
+                index={index}
+                todo={todo}
+                handleChangeTodo={handleCompleteChangeTodo}
+                handleDeleteTodo={handleCompleteDeleteTodo}
+              />
+            ))}
+            {provided.placeholder}
+          </section>
+        )}
+      </Droppable> 
+    </section>
+  );
+};
+
+export default TodoLists;
+
+
+const  SingleTodo=({ todo, handleDeleteTodo, handleChangeTodo, index }:SingleTodoProps) =>{
+ return (
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided) => (
+        <div className="flex rounded-md  justify-between  mt-3  p-[15px] bg-gray-400 hover:scale-[1.01]"
+       {...provided.dragHandleProps}
+       {...provided.draggableProps}
+       ref={provided.innerRef}
+        >
+         
+           {todo.isDone ? (
+            <s className="text-2xl font-light">{todo.text}</s>
+          ) : (
+             <span className="text-2xl">{todo.text}</span>
+          )}
+
+          <section className="flex ">
+            <button               className="cursor-pointer ml-3 text-xl"
+              onClick={() => {
+                handleDeleteTodo(todo.id);
+              }}
+             >
+               <AiFillDelete />
+             </button>
+            <button
+               className="cursor-pointer ml-3 text-xl"
+              onClick={() => {
+               
+                handleChangeTodo(todo.id);
+              }}
+            >
+              <MdDone />
+            </button>
+          </section>
+        </div>
+      )}
+    </Draggable>
+  );
+}
