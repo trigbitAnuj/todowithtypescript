16a33bb7c691f66d398ddc6e2a60f645b3f45517
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-18 17:59:24 +0530

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -2,18 +2,15 @@ import { Todo } from "./pages/TodoApp";
 
 export const getActiveTodosFromLocalStorage = ():Todo[] => {
   let todosList: Todo[] = JSON.parse(localStorage.getItem("activeTodos")||'[]');
-  if (todosList) {
+
     return todosList;
-  } else {
-    return [];
-  }
+ 
+  
 };
 
 export const getCompletedTodosFromLocalStorage = () => {
   let todosList:Todo[] = JSON.parse(localStorage.getItem("completedTodos")||'[]');
-  if (todosList) {
+  
     return todosList;
-  } else {
-    return [];
-  }
+  
 };
